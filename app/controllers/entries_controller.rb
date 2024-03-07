874ecd4edd1be11f0b513ec3c1d29b9d239ba429
Mark Buder <mark.buder@kellogg.northwestern.edu>
2024-03-07 23:49:01 +0000

    HW4- authorization & authentication complete

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,16 +1,27 @@
 class EntriesController < ApplicationController
 
+  def index
+      @posts = Post.all
+    end
+
   def new
   end
 
   def create
-    @entry = Entry.new
-    @entry["title"] = params["title"]
-    @entry["description"] = params["description"]
-    @entry["occurred_on"] = params["occurred_on"]
-    @entry["place_id"] = params["place_id"]
-    @entry.save
-    redirect_to "/places/#{@entry["place_id"]}"
+    @user = User.find_by({ "id" => session["user_id"] })
+    if @user != nil
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["occurred_on"] = params["occurred_on"]
+      @entry["place_id"] = params["place_id"]
+      @entry.save
+      redirect_to "/places/#{@entry["place_id"]}"
+    else
+      flash["notice"] = "Login first."
+    end
+    redirect_to "/login"
+  end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find_by({ "id" => params["id"] })
+  end
+  
   def new
   end
 
@@ -7,8 +11,8 @@ class UsersController < ApplicationController
     @user["first_name"] = params["first_name"]
     @user["last_name"] = params["last_name"]
     @user["email"] = params["email"]
-    @user["password"] = params["password"]
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
     @user.save
-    redirect_to "/"
+    redirect_to "/login"
   end
 end
