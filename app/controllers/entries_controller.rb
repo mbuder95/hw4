7fac577f8e2f5707d974ae5f44c74a95ed226d05
Mark Buder <mark.buder@kellogg.northwestern.edu>
2024-03-11 00:30:30 +0000

    HW 4- functional app

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -12,11 +12,11 @@ class EntriesController < ApplicationController
     if @user != nil
       @entry = Entry.new
       @entry["title"] = params["title"]
+      @entry.uploaded_image.attach(params["uploaded_image"])
       @entry["description"] = params["description"]
       @entry["occurred_on"] = params["occurred_on"]
       @entry["place_id"] = params["place_id"]
       @entry["user_id"] = @current_user["id"]
-      @entry.uploaded_image.attach(params["uploaded_image"])
       @entry.save
       redirect_to "/places/#{@entry["place_id"]}"
     else
